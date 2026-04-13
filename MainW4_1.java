f7ef082907898396ab4f17b41b3de97ce04eb835
youshush <yousephahmed1@gmail.com>
2026-04-13 13:09:53 +0100

    Add files via upload

---
+++ MainW4_1.java
@@ -0,0 +1,10 @@
+public class MainW4_1 {
+    public static void main (String[] args) {
+
+        int [] numbers = {10, 20, 30, 40, 50}; //array of numbers
+
+        for (int i = 0; i < numbers.length; i++) { //for loop
+            System.out.println(numbers[i]);
+        }
+    }
+}
